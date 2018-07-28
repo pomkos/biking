d12975e9fc00cba750da8a6df9196a2829d2ddb9
Russell Ratcliffe <rratcliffe57@gmail.com>
2018-07-28 10:09:02 -0400

    fixed peters errors

--- start_biking.py
+++ start_biking.py
@@ -5,29 +5,34 @@ import os
 
 #--- Options ---#
 
-#csv_file = "Bryan44" #name of csv file from the bike
+# csv_file = "Bryan44" #name of csv file from the bike
 
 #---------------#
 
+
 def reorg_excels():
-    path = 'C:\\Users\\albei\\Nextcloud\\Programming\\biking\\files' #where the csv files are located
-    all_files = glob.glob(os.path.join(path, "*.csv")) #make a list of paths
+    path = '/new_files'  # where the csv files are located
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))  # make a list of paths
 
     for files in all_files:
-        csv_file = os.path.splitext(os.path.basename(files))[0] #get file name without extension
+        csv_file = os.path.splitext(os.path.basename(files))[
+            0]  # get file name without extension
         extract_df(csv_file)
+
 
 def combine_excels():
     dataframes = []
-    path2 = 'C:\\Users\\albei\\Nextcloud\\Programming\\biking\\new_files'
-    new_files = glob.glob(os.path.join(path2, "*.csv"))
-    dataframes.append(new_files)
-    for f in [new_files]:
+    path2 = 'new_files'
+    new_files = glob.glob(os.path.join(path2, "*.xlsx"))
+    print(new_files)
+    # dataframes.append(new_files)
+    for f in new_files:
         data = pd.read_excel(f, 'Sheet1').iloc[:-2]
         data.index = [os.path.basename(f)] * len(data)
         dataframes.append(data)
 
     df = pd.concat(dataframes)
     print(df)
+
 
 combine_excels()

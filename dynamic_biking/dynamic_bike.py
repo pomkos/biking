a18765213e511321841ac5cc97e92f470dee56ce
pomkos <albeithome@gmail.com>
2018-07-31 16:33:30 -0400

    separate static/dynamic files, separate folders to make pyinstaller exe easier to differentiate

---
+++ dynamic_biking/dynamic_bike.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from pandas import ExcelWriter
+
+#--- Manipulate an Excel File ---#
+def extract_df(csv_file):
+    csv_file2 = 'dynamic_files\\' + csv_file + '.csv'
+    sheet = pd.read_csv(csv_file2, delimiter=',') #import excel page. Sheetname refers to sheet in file
+    tag = sheet.set_index(['Tag']) #set Tag column as the default index
+
+    # NOTE: use isin to iterate: tag.loc['item one','item two']
+    Power = tag.loc['{[CompactLogix]Actual_Power}'].set_index(';Date') #select all rows that have this tag
+    Power.rename(columns={'Value':'Power'}, inplace=True) #rename Value column to Power
+    Torque = tag.loc['{[CompactLogix]Actual_Torque}'].set_index(';Date')
+    Torque.rename(columns={'Value':'Torque'}, inplace=True)
+    HR = tag.loc['{[CompactLogix]Heart_Rate}'].set_index(';Date')
+    HR.rename(columns={'Value':'HR'}, inplace=True)
+    Minute = tag.loc['{[CompactLogix]Minute_Counter}'].set_index(';Date')
+    Minute.rename(columns={'Value':'Minute'}, inplace=True)
+    Second = tag.loc['{[CompactLogix]Second_Counter}'].set_index(';Date')
+    Second.rename(columns={'Value':'Second'}, inplace=True)
+    Cadence = tag.loc['{[CompactLogix]Rider_Cadence}'].set_index(';Date')
+    Cadence.rename(columns={'Value':'Cadence'}, inplace=True)
+
+    merge_df(Power, Torque, HR, Minute, Second, Cadence, csv_file)
+
+def merge_df(Power, Torque, HR, Minute, Second, Cadence, csv_file):
+    #--- Merge Dataframes ---#
+    #dfs=[Power,Torque,HR,Minute,Second,Cadence]
+    subject_data = pd.merge(Power,
+                    Torque[['Time','Torque']],
+                    on='Time')
+    subject_data = pd.merge(subject_data, #left dataframe to merge to
+                    HR[['Time','HR']], #Right dataframe: select two columns 
+                    on='Time') #merge based on matching "Time" column
+    subject_data = pd.merge(subject_data,
+                    Minute[['Time','Minute']],
+                    on='Time')
+    subject_data = pd.merge(subject_data,
+                    Second[['Time','Second']],
+                    on='Time')
+    subject_data = pd.merge(subject_data,
+                    Cadence[['Time','Cadence']],
+                    on='Time')
+
+    subject_data.insert(0,'Name',csv_file)
+
+    data_manip(subject_data, csv_file)
+
+def data_manip(subject_data, csv_file):
+    df = subject_data
+    df = df.drop(df[(df.Power <= 0) & (df.Cadence < 70)].index)
+    df = df.drop(df[(df.Marker == 'E') | (df.Marker == 'B')].index)
+    df = df.drop(df[(df.Minute == 0) & (df.Cadence <= 70)].index)
+    df = df.drop(df[(df.Minute == 5) & (df.Cadence <= 70)].index)
+    mask = df.HR > 180
+    column_name = 'HR'
+    df.loc[mask, column_name] = 0
+    mask = df.HR < 40
+    column_name = 'HR'
+    df.loc[mask, column_name] = 0
+    subject_data = df
+    save_excel(subject_data, csv_file)
+
+def save_excel(subject_data, csv_file):
+    csv_file = 'dynamic_new_files\\' + csv_file + '_new.xlsx'
+    #--- Convert Dataframe to Excel ---#
+    writer = ExcelWriter(csv_file)
+    subject_data.to_excel(writer,sheet_name='Sheet1')
+    writer.save()
---
+++ static_biking/start_sbiking.py
@@ -0,0 +1,36 @@
+from static_bike import extract_df
+
+import pandas as pd
+from pandas import ExcelWriter
+import glob
+import os
+
+def reorg_excels():
+    path = 'static_files' #where the csv files are located
+    all_files = glob.glob(os.path.join(path, "*.csv")) #make a list of paths
+
+    for files in all_files:
+        csv_file = os.path.splitext(os.path.basename(files))[0] #get file name without extension
+        extract_df(csv_file)
+        print(csv_file + ".csv reorganized!")
+
+def combine_excels():
+    dataframes = []
+    path2 = 'static_new_files'
+    new_files = glob.glob(os.path.join(path2, "*.xlsx"))
+    for f in new_files:
+        data = pd.read_excel(f, 'Sheet1').iloc[:-2]
+        data.index = [os.path.basename(f)] * len(data)
+        dataframes.append(data)
+
+    df = pd.concat(dataframes)
+    #save df as excel:
+    csv_file = 'static_sheets.xlsx'
+    #--- Convert Dataframe to Excel ---#
+    writer = ExcelWriter(csv_file)
+    df.to_excel(writer,sheet_name='Sheet1')
+    writer.save()
+    print(csv_file + " saved!")
+
+reorg_excels()
+combine_excels()
